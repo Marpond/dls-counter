0cc89d0d862dc5b48469c5ad3b588d0ba8e1bea8
feix0033 <evensnachi1@gmail.com>
2025-04-20 21:41:15 +0200

    fix lint

--- CounterService.Frontend/src/App.jsx
+++ CounterService.Frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import './App.css' 
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count] = useState(0)
 
   return (
     <>
