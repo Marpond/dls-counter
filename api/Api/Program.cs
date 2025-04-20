292237e38ff635be1cb7f648f4e2bd7f1063490f
Alvaro-rrdt <lucarrdtmail@gmail.com>
2025-04-20 15:28:59 +0200

    Github issue: Fix IAuthService issue when returning Results.Forbid() #1
    The /increment endpoint failed when the INCREMENT feature flag was disabled "INCREMENT=false" , the fix explicitly defines all possible responses either Ok response as expected when the feature flag is disabled and 403 "forbidden" when it is enable.
    
    To test this run the docker compose file and just make a post request to /increment and change the env INCREMENT to either "false" or "true" (careful with spaces needs to be clean) and you will see the desired outputs.

--- api/Api/Program.cs
+++ api/Api/Program.cs
@@ -9,20 +9,22 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(_ => ConnectionMultiplexer
 var app = builder.Build();
 
 app.MapPost("/increment", async (IConnectionMultiplexer redis) =>
-{
-    var enabled = bool.TryParse(
-        Environment.GetEnvironmentVariable("INCREMENT"), 
-        out var flag
-    ) && flag;
+    {
+        var enabled = bool.TryParse(
+            Environment.GetEnvironmentVariable("INCREMENT"),
+            out var flag
+        ) && flag;
 
-    if (!enabled)
-        return Results.Forbid();
+        if (!enabled)
+            return Results.StatusCode(StatusCodes.Status403Forbidden);
 
-    var db = redis.GetDatabase();
-    var newValue = await db.StringIncrementAsync("counter");
-    return Results.Ok(newValue);
-})
-.WithName("Increment")
-.WithOpenApi();
+        var db = redis.GetDatabase();
+        var newValue = await db.StringIncrementAsync("counter");
+        return Results.Ok(newValue);
+    })
+    .WithName("Increment")
+    .WithOpenApi()
+    .Produces<long>(StatusCodes.Status200OK) //the default response is 200 OK, but I explicitly write it here for improved readability.
+    .Produces(StatusCodes.Status403Forbidden);
 
 app.Run();
