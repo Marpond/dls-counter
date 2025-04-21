68d984ab99998527d0a92dca7cc7a08e05635c08
feix0033 <evensnachi1@gmail.com>
2025-04-21 11:04:59 +0200

    fix linting error

--- CounterService.Frontend/src/tests/init.test.jsx
+++ CounterService.Frontend/src/tests/init.test.jsx
@@ -1,5 +1,5 @@
-import { test, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { test } from 'vitest'
+import { render } from '@testing-library/react'
 import App from '../App'
 
 test('should run test', () => {
