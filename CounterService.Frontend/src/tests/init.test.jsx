61fb304d9b43987b5a5d9ea3644d56709621f649
feix0033 <evensnachi1@gmail.com>
2025-04-21 11:08:56 +0200

    try again

--- CounterService.Frontend/src/tests/init.test.jsx
+++ CounterService.Frontend/src/tests/init.test.jsx
@@ -1,7 +1,9 @@
-import { test } from 'vitest'
-import { render } from '@testing-library/react'
+import { test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
 import App from '../App'
 
 test('should run test', () => {
 	render(<App />)
+	const title = screen.getByText(/Counter/)
+	expect(title).not.toBeNull()
 })
